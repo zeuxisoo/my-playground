a78674f69463c778fa287ded63afbec626f8280b
Zeuxis Lo <seekstudio@gmail.com>
2025-06-03 22:22:00 +0800

    0023: using map instead of (name, value) pairs in Tool enum

--- 0023-patch-roots-save-any/python/kind.py
+++ 0023-patch-roots-save-any/python/kind.py
@@ -9,22 +9,22 @@ Quality = Enum('Quality', [
     ('Best', 3),
 ])
 
-Tool = Enum('Tool', [
-    ('None', 0),
-	('Handaxe', 100),
-	('Axe', 200),
-	('Hamme', 300),
-	('Wate', 400),
-	('Hoe', 500),
-	('Sickle', 600),
-	('Shears', 700),
-	('Milke', 800),
-	('Brush', 900),
-	('Harpoon', 1000),
-	('FishingRod', 1100),
-	('Flute', 1200),
-	('Torch', 1300),
-])
+Tool = Enum('Tool', {
+    'None': 0, # Tool['None']
+	'Handaxe': 100,
+	'Axe': 200,
+	'Hamme': 300,
+	'Wate': 400,
+	'Hoe': 500,
+	'Sickle': 600,
+	'Shears': 700,
+	'Milke': 800,
+	'Brush': 900,
+	'Harpoon': 1000,
+	'FishingRod': 1100,
+	'Flute': 1200,
+	'Torch': 1300,
+})
 
 class Entity(Enum):
     Plant = 0
