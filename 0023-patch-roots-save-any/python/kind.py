b7b27b5cd84da60697fc58e89b6ebd55cded052c
Zeuxis Lo <seekstudio@gmail.com>
2025-06-03 22:17:55 +0800

    0023: add entity enum

--- 0023-patch-roots-save-any/python/kind.py
+++ 0023-patch-roots-save-any/python/kind.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from enum import Enum
+from enum import Enum, auto
 
 Quality = Enum('Quality', [
     ('Normal', 0),
@@ -25,3 +25,62 @@ Tool = Enum('Tool', [
 	('Flute', 1200),
 	('Torch', 1300),
 ])
+
+class Entity(Enum):
+    Plant = 0
+    Tree = auto()
+    Resource = auto()
+    MagneticItem = auto()
+    Player = auto()
+    Inventory = auto()
+    SeedInventory = auto()
+    Currency = auto()
+    ShippingBin = auto()
+    NPC = auto()
+    Weather = auto()
+    Animal = auto()
+    AnimalHerd = auto()
+    GrassStarter = auto()
+    Bed = auto()
+    WildPlantPatch = auto()
+    IrrigationPump = auto()
+    PachaStatue = auto()
+    Nanny = auto()
+    Idea = auto()
+    Building = auto()
+    TallGrass = auto()
+    PickableItem = auto()
+    TileableDecoration = auto()
+    FenceDoor = auto()
+    WaterWell = auto()
+    Decoration = auto()
+    BeeHive = auto()
+    RacingLeague = auto()
+    ReflectionStatue = auto()
+    FirePit = auto()
+    Score = auto()
+    ManualProducer = auto()
+    AutoProducer = auto()
+    StorageBox = auto()
+    Dashboard = auto()
+    SchoolWorkshop = auto()
+    PlantNursery = auto()
+    FollowingPetData = auto()
+    QuestManager = auto()
+    Calendar = auto()
+    IrrigationTrench = auto()
+    IrrigationExtender = auto()
+    TotemOffering = auto()
+    CaveRoom = auto()
+    CaveTorch = auto()
+    CavePaint = auto()
+    Trading = auto()
+    LimitedStockShop = auto()
+    MainStory = auto()
+    UniqueDataManager = auto()
+    Fence = auto()
+    ProsperityObjects = auto()
+    Butcher = auto()
+    TrapSpot = auto()
+    ItemGrower = auto()
+    Olla = auto()
